27258caf0d8bbff15185473f4874a7e4db252bf7
Neha Lingareddy <32804892+wookiee2187@users.noreply.github.com>
2020-04-09 12:57:00 -0400

    Update submit_transformation_request.py

--- servicex/resources/submit_transformation_request.py
+++ servicex/resources/submit_transformation_request.py
@@ -38,7 +38,7 @@ from flask_restful import reqparse
 from servicex.models import TransformRequest, DatasetFile, db
 from servicex.resources.servicex_resource import ServiceXResource
 from werkzeug.exceptions import BadRequest
-from flask_jwt_extended import (create_access_token, create_refresh_token, jwt_required, verify_jwt_in_request, jwt_refresh_token_required, get_jwt_identity)
+from flask_jwt_extended import jwt_required
 
 
 parser = reqparse.RequestParser()
