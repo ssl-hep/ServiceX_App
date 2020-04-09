3fe256a81082fe10b2ee5081377e36b12ce64988
Neha Lingareddy <32804892+wookiee2187@users.noreply.github.com>
2020-04-09 12:59:48 -0400

    Update models.py

--- servicex/models.py
+++ servicex/models.py
@@ -212,15 +212,13 @@ class DatasetFile(db.Model):
     def get_path_id(self):
         return self.request_id+":"+str(self.id)
 
-#from servicex.__init__ import db
-from passlib.hash import pbkdf2_sha256 as sha256
 
 class UserModel(db.Model):
     __tablename__ = 'users'
 
     id = db.Column(db.Integer, primary_key = True)
-    username = db.Column(db.String(120), unique = True, nullable = False)
-    password = db.Column(db.String(120), nullable = False)
+    username = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(120), nullable=False)
 
     def save_to_db(self):
         db.session.add(self)
@@ -228,7 +226,7 @@ class UserModel(db.Model):
 
     @classmethod
     def find_by_username(cls, username):
-        return cls.query.filter_by(username = username).first()
+        return cls.query.filter_by(username=username).first()
 
     @classmethod
     def return_all(cls):
