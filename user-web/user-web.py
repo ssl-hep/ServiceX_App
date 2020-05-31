f4410f811e0f3f115fcdaa90964f69fd38712175
wookiee2187 <nlingareddy@uchicago.edu>
2020-05-30 20:39:58 -0400

    cleaner

--- user-web/user-web.py
+++ user-web/user-web.py
@@ -1,1 +1,10 @@
-from app import app
+from flask import Flask, render_template
+
+app = Flask(__name__)
+@app.route('/')
+@app.route('/index', methods=['GET', 'POST'])
+def index():
+    return render_template('index.html', title='Home')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=80)
