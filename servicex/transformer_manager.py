818751b57d000a4779032c6df0eda51d65b8541f
Ilija Vukotic <ivukotic@cern.ch>
2021-04-05 15:58:35 -0500

    removal of generated-source CM once transform is gone.

--- servicex/transformer_manager.py
+++ servicex/transformer_manager.py
@@ -227,6 +227,11 @@ class TransformerManager:
             namespace=namespace
         )
 
+        api_v1 = client.CoreV1Api()
+        configmap_name = "{}-generated-source".format(request_id)
+        api_v1.delete_namespaced_config_map(name=configmap_name,
+                                            namespace=namespace)
+
     @staticmethod
     def create_configmap_from_zip(zipfile, request_id, namespace):
         configmap_name = "{}-generated-source".format(request_id)
