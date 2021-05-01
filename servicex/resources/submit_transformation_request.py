ssl-hep/ServiceX_App

commit bc2696f8bd0740650208a3f7a2284a03985ce98b

Refactor SubmitTransformationRequest to be a little more Pythonic

servicex/resources/submit_transformation_request.py
@@ -96,67 +96,60 @@ class SubmitTransformationRequest(ServiceXResource):
     @auth_required
     def post(self):
         try:
-            transformation_request = parser.parse_args()
+            args = parser.parse_args()
+            config = current_app.config
             print("object store ", self.object_store)
 
             request_id = str(uuid.uuid4())
-            time = datetime.now(tz=timezone.utc)
+            image = args.get("image", config['TRANSFORMER_DEFAULT_IMAGE'])
+            did = args.get("did")
+            file_list = args.get("file-list")
 
-            transformer_image = transformation_request['image'] \
-                if transformation_request['image'] \
-                else current_app.config['TRANSFORMER_DEFAULT_IMAGE']
-
-            requested_did = transformation_request['did'] \
-                if 'did' in transformation_request else None
-            requested_file_list = transformation_request['file-list'] \
-                if 'file-list' in transformation_request else None
-
-            # requested_did xor requested_file_list
-            if bool(requested_did) == bool(requested_file_list):
+            # did xor file_list
+            if bool(did) == bool(file_list):
                 raise BadRequest("Must provide did or file-list but not both")
 
             if self.object_store and \
-                    transformation_request['result-destination'] == \
+                    args['result-destination'] == \
                     TransformRequest.OBJECT_STORE_DEST:
                 self.object_store.create_bucket(request_id)
                 # WHat happens if object-store and object_store is None?
 
-            if transformation_request['result-destination'] == TransformRequest.KAFKA_DEST:
-                broker = transformation_request['kafka']['broker']
+            if args['result-destination'] == TransformRequest.KAFKA_DEST:
+                broker = args['kafka']['broker']
             else:
                 broker = None
 
-            if current_app.config['TRANSFORMER_VALIDATE_DOCKER_IMAGE']:
-                tagged_image = transformer_image
-                if not self.docker_repo_adapter.check_image_exists(tagged_image):
-                    return {'message': f"The requested transformer docker image doesn't exist: {tagged_image}"}, 400  # noqa: E501
+            if config['TRANSFORMER_VALIDATE_DOCKER_IMAGE']:
+                if not self.docker_repo_adapter.check_image_exists(image):
+                    msg = f"Requested transformer docker image doesn't exist: {image}"
+                    return {'message': msg}, 400
 
+            user = self.get_requesting_user()
             request_rec = TransformRequest(
-                did=requested_did if requested_did else "File List Provided in Request",
-                submit_time=time,
-                columns=transformation_request['columns'],
-                selection=transformation_request['selection'],
-                tree_name=transformation_request['tree-name'],
+                did=did if did else "File List Provided in Request",
+                submit_time=datetime.now(tz=timezone.utc),
+                submitted_by=user.id if user is not None else None,
+                columns=args['columns'],
+                selection=args['selection'],
+                tree_name=args['tree-name'],
                 request_id=str(request_id),
-                image=transformer_image,
-                chunk_size=transformation_request['chunk-size'],
-                result_destination=transformation_request['result-destination'],
-                result_format=transformation_request['result-format'],
+                image=image,
+                chunk_size=args['chunk-size'],
+                result_destination=args['result-destination'],
+                result_format=args['result-format'],
                 kafka_broker=broker,
-                workers=transformation_request['workers'],
-                workflow_name=_workflow_name(transformation_request),
+                workers=args['workers'],
+                workflow_name=_workflow_name(args),
                 status='Submitted',
                 app_version=self._get_app_version(),
-                code_gen_image=current_app.config['CODE_GEN_IMAGE']
+                code_gen_image=config['CODE_GEN_IMAGE']
             )
-            user = self.get_requesting_user()
-            if user:
-                request_rec.submitted_by = user.id
 
             # If we are doing the xaod_cpp workflow, then the first thing to do is make
             # sure the requested selection is correct, and generate the C++ files
             if request_rec.workflow_name == 'selection_codegen':
-                namespace = current_app.config['TRANSFORMER_NAMESPACE']
+                namespace = config['TRANSFORMER_NAMESPACE']
                 request_rec.generated_code_cm = \
                     self.code_gen_service.generate_code_for_selection(request_rec, namespace)
 
@@ -186,7 +179,7 @@ class SubmitTransformationRequest(ServiceXResource):
             request_rec.save_to_db()
             db.session.commit()
 
-            if requested_did:
+            if did:
                 did_request = {
                     "request_id": request_rec.request_id,
                     "did": request_rec.did,
@@ -196,12 +189,12 @@ class SubmitTransformationRequest(ServiceXResource):
                     )
                 }
 
-                parsed_did = DIDParser(requested_did,
-                                       default_scheme=current_app.config[
-                                           'DID_FINDER_DEFAULT_SCHEME'])
-
-                if parsed_did.scheme not in current_app.config['VALID_DID_SCHEMES']:
-                    return {'message': f"The requested DID scheme is not supported: {parsed_did.scheme}"}, 400  # noqa: E501
+                parsed_did = DIDParser(
+                    did, default_scheme=config['DID_FINDER_DEFAULT_SCHEME']
+                )
+                if parsed_did.scheme not in config['VALID_DID_SCHEMES']:
+                    msg = f"DID scheme is not supported: {parsed_did.scheme}"
+                    return {'message': msg}, 400
 
                 self.rabbitmq_adaptor.basic_publish(exchange='',
                                                     routing_key=parsed_did.microservice_queue,
@@ -210,9 +203,9 @@ class SubmitTransformationRequest(ServiceXResource):
                 # Request a preflight check on the first file
                 self.lookup_result_processor.publish_preflight_request(
                     request_rec,
-                    requested_file_list[0])
+                    file_list[0])
 
-                for file_path in requested_file_list:
+                for file_path in file_list:
                     file_record = DatasetFile(request_id=request_id,
                                               file_path=file_path,
                                               adler32="xxx",
@@ -225,7 +218,7 @@ class SubmitTransformationRequest(ServiceXResource):
 
                 self.lookup_result_processor.report_fileset_complete(
                     request_rec,
-                    num_files=len(requested_file_list)
+                    num_files=len(file_list)
                 )
 
                 db.session.commit()
@@ -234,7 +227,8 @@ class SubmitTransformationRequest(ServiceXResource):
                 "request_id": str(request_id)
             }
         except BadRequest as bad_request:
-            return {'message': f'The json request was malformed: {str(bad_request)}'}, 400
+            msg = f'The json request was malformed: {str(bad_request)}'
+            return {'message': msg}, 400
         except ValueError as eek:
             return {'message': f'Failed to submit transform request: {str(eek)}'}, 400
         except Exception:

tests/resources/test_submit_transformation_request.py
@@ -94,6 +94,7 @@ class TestSubmitTransformationRequest(ResourceTestBase):
         response = client.post('/servicex/transformation', json=request)
 
         assert response.status_code == 400
+        assert "DID scheme is not supported" in response.json["message"]
 
     def test_submit_transformation_request_throws_exception(
         self, mocker, mock_rabbit_adaptor
@@ -257,7 +258,7 @@ class TestSubmitTransformationRequest(ResourceTestBase):
         response = client.post('/servicex/transformation',
                                json=self._generate_transformation_request())
         assert response.status_code == 400
-        assert response.json == {"message": "The requested transformer docker image doesn't exist: ssl-hep/foo:latest"}  # noqa: E501
+        assert response.json == {"message": "Requested transformer docker image doesn't exist: ssl-hep/foo:latest"}  # noqa: E501
 
     def test_submit_transformation_request_no_docker_check(
         self, mocker, mock_docker_repo_adapter

commit ba7f1537db362285c399143e064081e2588a86c4

Fix bug introduced in #104 and add update tests to prevent regression

servicex/resources/submit_transformation_request.py
@@ -52,7 +52,7 @@ parser.add_argument(
 )
 parser.add_argument('columns', help='This field cannot be blank')
 parser.add_argument('selection', help='Query string')
-parser.add_argument('image')
+parser.add_argument('image', default=current_app.config['TRANSFORMER_DEFAULT_IMAGE'])
 parser.add_argument('tree-name')
 parser.add_argument('chunk-size', type=int)
 parser.add_argument('workers', type=int)
@@ -100,7 +100,7 @@ class SubmitTransformationRequest(ServiceXResource):
             print("object store ", self.object_store)
 
             request_id = str(uuid.uuid4())
-            image = args.get("image", config['TRANSFORMER_DEFAULT_IMAGE'])
+            image = args["image"]
             did = args.get("did")
             file_list = args.get("file-list")
 

tests/resources/test_submit_transformation_request.py
@@ -28,6 +28,8 @@
 import json
 from unittest.mock import call
 
+from flask import current_app
+
 from servicex import LookupResultProcessor
 from servicex.models import TransformRequest
 from tests.resource_test_base import ResourceTestBase
@@ -40,7 +42,6 @@ class TestSubmitTransformationRequest(ResourceTestBase):
         request = {
             'did': '123-45-678',
             'columns': "e.e, e.p",
-            'image': 'ssl-hep/foo:latest',
             'result-destination': 'kafka',
             'kafka': {'broker': 'ssl.hep.kafka:12332'},
             'chunk-size': 500,
@@ -119,7 +120,7 @@ class TestSubmitTransformationRequest(ResourceTestBase):
             assert saved_obj.request_id == request_id
             assert saved_obj.title is None
             assert saved_obj.columns == "e.e, e.p"
-            assert saved_obj.image == 'ssl-hep/foo:latest'
+            assert saved_obj.image == current_app.config["TRANSFORMER_DEFAULT_IMAGE"]
             assert saved_obj.chunk_size == 500
             assert saved_obj.workers == 10
             assert saved_obj.result_destination == 'kafka'
@@ -240,9 +241,10 @@ class TestSubmitTransformationRequest(ResourceTestBase):
         mock_docker_repo_adapter.check_image_exists = mocker.Mock(return_value=False)
         client = self._test_client(docker_repo_adapter=mock_docker_repo_adapter)
         request = self._generate_transformation_request()
+        request["image"] = "ssl-hep/foo:latest"
         response = client.post('/servicex/transformation', json=request)
         assert response.status_code == 400
-        assert response.json == {"message": "Requested transformer docker image doesn't exist: ssl-hep/foo:latest"}  # noqa: E501
+        assert response.json == {"message": "Requested transformer docker image doesn't exist: " + request["image"]}  # noqa: E501
 
     def test_submit_transformation_request_no_docker_check(
         self, mocker, mock_docker_repo_adapter