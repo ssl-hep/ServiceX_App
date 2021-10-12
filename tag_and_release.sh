69c75c6388a8127a1a39ac0d4f41e95ef400016c
Ben Galewsky <ben@peartreestudio.net>
2021-10-12 13:41:16 -0500

    Tag and release updates the app version

--- tag_and_release.sh
+++ tag_and_release.sh
@@ -8,5 +8,10 @@ docker build -t sslhep/servicex_app:stage .
 docker run --rm --entrypoint "pip" sslhep/servicex_app:stage freeze > requirements.txt
 docker rmi sslhep/servicex_app:stage
 git add requirements.txt
+
+sed s"/version='.*'/version='$1'/" setup.py > new_setup.py
+mv new_setup.py setup.py
+git add setup.py
+
 git commit -m "Freeze dependencies for V$1"
 git push origin v$1
