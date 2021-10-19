efc9191b6790e2fdaf642ba3b7318d8cd00f72e0
Suchandra Thapa <suchandra@gmail.com>
2021-10-19 08:45:26 -1000

    Change log message to debug level

--- servicex/web/transformation_request.py
+++ servicex/web/transformation_request.py
@@ -6,7 +6,7 @@ from servicex.models import TransformRequest
 
 @oauth_required
 def transformation_request(id_: str):
-    current_app.logger.info(f"Got transformation request: {id_}")
+    current_app.logger.debug(f"Got transformation request: {id_}")
     req = TransformRequest.lookup(id_)
     if not req:
         abort(404)
